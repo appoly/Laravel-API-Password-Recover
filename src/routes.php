ffd94819a3ada2b10d4cd62eb50dcf4340bfedd6
James Merrix <james@appoly.co.uk>
2020-01-03 10:57:32 +0000

    Formatting.

--- src/routes.php
+++ src/routes.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('api/forgot-password', 'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@forgot');
-    Route::post('api/forgot-password', 'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@reset');
+Route::post('api/forgot-password', 'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@reset');
