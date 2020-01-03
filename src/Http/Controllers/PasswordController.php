ee12a37bc4551a7183256aab1b613b4ddaf2074c
James Merrix <james@appoly.co.uk>
2020-01-03 11:03:33 +0000

    Removed an uneeded else.

--- src/Http/Controllers/PasswordController.php
+++ src/Http/Controllers/PasswordController.php
@@ -10,12 +10,13 @@ class PasswordController
     public function forgot(Request $request)
     {
         if ($request->has('email')) {
-            
+
             $password_helper_key = str_pad(mt_rand(0, 999999), 6, '0', STR_PAD_LEFT);
 
             $user = \App\User::where('email', $request->email)->first();
 
             if ($user) {
+
                 $user->password_helper_key = $password_helper_key;
                 $user->save();
 
@@ -38,7 +39,9 @@ class PasswordController
     public function reset(Request $request)
     {
         if ($request->has('key') && $request->has('password')) {
+
             $user = \App\User::where('password_helper_key', $request->key)->first();
+            
             if ($user) {
                 $user->update([
                     'password' => bcrypt($request->password),
@@ -53,10 +56,11 @@ class PasswordController
                     ['message' => 'No user found'],
                 ], 400);
             }
-        } else {
-            return response()->json([
-                ['message' => 'Key and Password are required'],
-            ], 400);
         }
+        
+        return response()->json([
+            ['message' => 'Key and Password are required'],
+        ], 400);
+        
     }
 }
