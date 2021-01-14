347cb4537da1ece9b98fa6de8cc50a6102f1667c
John Wedgbury <johnwedgbury@users.noreply.github.com>
2021-01-14 12:26:46 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/PasswordController.php
+++ src/Http/Controllers/PasswordController.php
@@ -11,7 +11,7 @@ class PasswordController
     {
         $userModel = config('auth.providers.users.model');
         $userClass = new $userModel;
-        
+
         if ($request->has('email')) {
             $password_helper_key = str_pad(mt_rand(0, 999999), config('LaravelApiPasswordHelper.PASSWORD_RESET_CODE_LENGTH'), '0', STR_PAD_LEFT);
 
@@ -42,7 +42,7 @@ class PasswordController
     {
         $userModel = config('auth.providers.users.model');
         $userClass = new $userModel;
-        
+
         if ($request->has('key') && $request->has('password')) {
             $user = $userClass::where('password_helper_key', $request->key)->first();
 
