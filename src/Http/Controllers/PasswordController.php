ea36ef1613eeac70eb0a30599ea8013fd8121883
John Wedgbury <john@appoly.co.uk>
2021-01-14 12:26:39 +0000

    Update PasswordController.php

--- src/Http/Controllers/PasswordController.php
+++ src/Http/Controllers/PasswordController.php
@@ -7,17 +7,15 @@ use Illuminate\Http\Request;
 
 class PasswordController
 {
-    public function __construct()
-    {
-        class_alias(config('auth.providers.users.model'), 'User');
-    }
-
     public function forgot(Request $request)
     {
+        $userModel = config('auth.providers.users.model');
+        $userClass = new $userModel;
+        
         if ($request->has('email')) {
             $password_helper_key = str_pad(mt_rand(0, 999999), config('LaravelApiPasswordHelper.PASSWORD_RESET_CODE_LENGTH'), '0', STR_PAD_LEFT);
 
-            $user = User::where('email', $request->email)
+            $user = $userClass::where('email', $request->email)
                 ->orWhere('email', urldecode($request->email))
                 ->first();
 
@@ -42,8 +40,11 @@ class PasswordController
 
     public function reset(Request $request)
     {
+        $userModel = config('auth.providers.users.model');
+        $userClass = new $userModel;
+        
         if ($request->has('key') && $request->has('password')) {
-            $user = User::where('password_helper_key', $request->key)->first();
+            $user = $userClass::where('password_helper_key', $request->key)->first();
 
             if ($user) {
                 $user->update([
