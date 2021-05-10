cf0a82339e07fecf6acf46b45df51f5a2e4ee0c0
John Wedgbury <johnwedgbury@users.noreply.github.com>
2021-05-10 13:35:41 +0000

    Apply fixes from StyleCI

--- src/routes.php
+++ src/routes.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-if(!Route::has('appoly.api.forgot-password')) {
+if (! Route::has('appoly.api.forgot-password')) {
     Route::get(
         'api/forgot-password',
         'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@forgot'
     )->name('appoly.api.forgot-password');
 }
 
-if(!Route::has('appoly.api.submit-forgot-password')) {
+if (! Route::has('appoly.api.submit-forgot-password')) {
     Route::post(
         'api/forgot-password',
         'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@reset'
