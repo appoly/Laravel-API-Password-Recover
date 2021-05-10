ad4c38fb4d245095e5e3bf2384a0b7a5113d30d3
John Wedgbury <john@appoly.co.uk>
2021-05-10 14:35:31 +0100

    Allow routes to be overridden

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('api/forgot-password', 'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@forgot');
-Route::post('api/forgot-password', 'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@reset');
+if(!Route::has('appoly.api.forgot-password')) {
+    Route::get(
+        'api/forgot-password',
+        'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@forgot'
+    )->name('appoly.api.forgot-password');
+}
+
+if(!Route::has('appoly.api.submit-forgot-password')) {
+    Route::post(
+        'api/forgot-password',
+        'Appoly\LaravelApiPasswordHelper\Http\Controllers\PasswordController@reset'
+    )->name('appoly.api.submit-forgot-password');
+}
